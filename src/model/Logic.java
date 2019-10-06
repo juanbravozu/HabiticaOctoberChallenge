28fa44631df5f2e9c09e6226aced1c681887004e
Juan Bravo <juanda.bravo98@gmail.com>
2019-10-06 18:21:57 -0500

    Data base Added

--- src/model/Logic.java
+++ src/model/Logic.java
@@ -1,12 +1,18 @@
 package model;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class Logic {
 
 	private PApplet app;
+	private String[][] wordBank = new String[10][];
 	
 	public Logic(PApplet app) {
 		this.app = app;
+		for (int i = 0; i < 10; i++) {
+			wordBank[i] = app.loadStrings((2+i)+"letterWords.txt");
+		}
 	}
 }
